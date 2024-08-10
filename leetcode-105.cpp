4af20ce75ef2f9ff9b9c418c678f38263e47d7f2
leozzmc <zzmczzmczzmc870125@gmail.com>
2024-08-10 11:28:30 +0800

    LeetCode-105

--- leetcode-105.cpp
+++ leetcode-105.cpp
@@ -1,58 +1,88 @@
-# include <iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 class BT;
-class TreeNode{
-    public:
-        int val;
-        TreeNode *left,*right;
-        TreeNode():val(0),left(nullptr),right(nullptr) {};
-        TreeNode(int x):val(x),left(nullptr),right(nullptr) {};
-        TreeNode(int x, TreeNode *leftNode, TreeNode *rightNode):val(x),left(leftNode),right(rightNode) {};
+class TreeNode {
+public:
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* leftNode, TreeNode* rightNode) : val(x), left(leftNode), right(rightNode) {}
     friend class BT;
 };
 
-class BT{
-    public:
-        TreeNode *root = new TreeNode;
-        BT():root(nullptr) {};
-        BT(TreeNode *node):root(node) {};
+class BT {
+public:
+    TreeNode* root;
+    vector<int> preorder_list;
+    vector<int> inorder_list;
 
-        //memeber functions
-        void preorder(TreeNode *current);
+    BT() : root(nullptr) {}
+    BT(TreeNode* node) : root(node) {}
 
-        
+    // member functions
+    void preorder(TreeNode* current);
+    void inorder(TreeNode* current);
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder);
+    TreeNode* buildTreeHelper(const std::vector<int>& preorder, int preStart, int preEnd,
+                              const std::vector<int>& inorder, int inStart, int inEnd);
 };
 
-void BT::preorder(TreeNode *current){
-    if(current==NULL) return;
+TreeNode* BT::buildTreeHelper(const std::vector<int>& preorder, int preStart, int preEnd,
+                              const std::vector<int>& inorder, int inStart, int inEnd) {
+    if (preStart > preEnd || inStart > inEnd) return nullptr;
+
+    int rootVal = preorder[preStart];
+    TreeNode* root = new TreeNode(rootVal);
+
+    int mid;
+    for (mid = inStart; mid <= inEnd; mid++) {
+        if (inorder[mid] == rootVal) {
+            break;
+        }
+    }
+
+    int leftTreeSize = mid - inStart;
+
+    root->left = buildTreeHelper(preorder, preStart + 1, preStart + leftTreeSize, inorder, inStart, mid - 1);
+    root->right = buildTreeHelper(preorder, preStart + leftTreeSize + 1, preEnd, inorder, mid + 1, inEnd);
+
+    return root;
+}
+
+TreeNode* BT::buildTree(vector<int>& preorder, vector<int>& inorder) {
+    if (preorder.empty() || inorder.empty() || preorder.size() != inorder.size()) return nullptr;
+
+    return buildTreeHelper(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1);
+}
+
+void BT::preorder(TreeNode* current) {
+    if (current == nullptr) return;
     cout << current->val << " ";
     preorder(current->left);
     preorder(current->right);
 }
+void BT::inorder(TreeNode* current) {
+    if (current == nullptr) return;
+    
+    inorder(current->left);
+    cout << current->val << " ";
+    inorder(current->right);
+}
 
-int main(){
 
-    TreeNode *nodeA = new TreeNode(1);
-    TreeNode *nodeB = new TreeNode(2);
-    TreeNode *nodeC = new TreeNode(3);
-    TreeNode *nodeD = new TreeNode(4);
-    TreeNode *nodeE = new TreeNode(5);
-    TreeNode *nodeF = new TreeNode(6);
-    TreeNode *nodeG = new TreeNode(7);
+int main() {
+    BT T;
 
-    nodeA->left = nodeB;
-    nodeA->right = nodeC;
-    nodeB->left = nodeD;
-    nodeB->right = nodeE;
-    nodeC->right nodeF;
-    nodeE ->left = nodeG;
-
-    BT T(nodeA);
-
-    cout << "pre-order traversal" << endl;
+    vector<int> preorder_list = {3, 9, 20, 15, 7};
+    vector<int> inorder_list = {9, 3, 15, 20, 7};
+    T.root = T.buildTree(preorder_list, inorder_list);
+    cout << "preorder:" << endl;
     T.preorder(T.root);
-
-
+    cout << "inorder:" << endl; 
+    T.inorder(T.root);
     return 0;
 }
